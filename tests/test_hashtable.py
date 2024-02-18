77413af18b003af3921bb0d7472c64c5b819c6cc
Mohammed Jennane <mohammed.jennane@axa.com>
2024-02-18 19:08:43 +0100

    failure

--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -5,6 +5,9 @@ from pythonTDD.sources.hashtable import HashTable
 def test_should_always_pass():
     assert 2+2 == 4, "test_should_always_pass did not pass"
 
+def test_should_always_fail():
+    assert 2+2 == 22, "test_should_always_fail did not fail"
+
 
 def test_create_hashtable():
     assert HashTable() is not None
