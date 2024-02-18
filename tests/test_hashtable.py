38eb9a922331b5c2890fa0a483dedf2effde41e5
Mohammed Jennane <mohammed.jennane@axa.com>
2024-02-18 19:17:12 +0100

    no more test failures

--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -5,8 +5,8 @@ from pythonTDD.sources.hashtable import HashTable
 def test_should_always_pass():
     assert 2+2 == 4, "test_should_always_pass did not pass"
 
-def test_should_always_fail():
-    assert 2+2 == 22, "test_should_always_fail did not fail"
+# def test_should_always_fail():
+#     assert 2+2 == 22, "test_should_always_fail did fail"
 
 
 def test_create_hashtable():
